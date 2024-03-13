d152a000a1a77ca6941121083014afaa774a2797
Kornel Szarapajew <krnlszarapajew7@gmail.com>
2024-03-13 23:22:47 +0100

    Composite

--- src/main/java/com/example/labjee/seeders/GenreSeeder.java
+++ src/main/java/com/example/labjee/seeders/GenreSeeder.java
@@ -1,30 +1,26 @@
 package com.example.labjee.seeders;
 
 import com.example.labjee.helpers.GenresPrototype;
-import com.example.labjee.models.Genre;
 import com.example.labjee.repositories.GenreRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+// Tydzień 3 - wzorzec Composite - klasa będąca liściem interfejsu Seeder, odpowiedzialnym za wypełnianie tablicy gatunków
 @Component
-public class GenreSeeder implements CommandLineRunner {
+public class GenreSeeder implements Seeder {
     @Autowired
     GenreRepository genreRepository;
 
-    @Override
-    public void run(String... args) throws Exception {
-        seed();
-    }
-
     // Tydzień 2 - wzorzec Prototype - zastosowanie 1
-    private void seed() {
-        if (genreRepository.count() == 0) {
+    @Override
+    public void seed() {
+        if (this.genreRepository.count() == 0) {
             GenresPrototype genres = new GenresPrototype();
             genres.loadData();
 
-            genreRepository.saveAll(genres.getGenres());
+            this.genreRepository.saveAll(genres.getGenres());
         }
     }
     // Tydzień 2 - wzorzec Prototype - zastosowanie 1 - koniec
 }
+// Tydzień 3 - wzorzec Composite - koniec
