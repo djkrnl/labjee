481eaf2ca857fb346939f29bab27893fe0bfedca
w-sarnowski <sarnowskiwojtek5500@gmail.com>
2024-04-07 02:39:45 +0200

    Mediator

--- src/main/java/com/example/labjee/config/WebSecurityConfig.java
+++ src/main/java/com/example/labjee/config/WebSecurityConfig.java
@@ -6,9 +6,13 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Properties;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -32,6 +36,26 @@ import org.springframework.web.servlet.support.SessionFlashMapManager;
 public class WebSecurityConfig {
     @Autowired
     private UserDetailsServiceImpl userDetailsService;
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername("jeesender@gmail.com");
+        mailSender.setPassword("cXT87FB6}g+6");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+        props.put("mail.from.email", "jeesender@gmail.com");
+        mailSender.setJavaMailProperties(props);
+
+        return mailSender;
+    }
     
     @Bean
     public AuthenticationManager authenticationManager() throws Exception {
